8cec3f7fcb89ef91be725073462b0553383c5401
Abdul Alim Baig <abdul.baig@frieslandcampina.com>
2023-12-27 14:12:51 +0530

    new changes made

--- template.py
+++ template.py
@@ -17,7 +17,7 @@ list_of_files = [
     f"src/{project_name}/pipeline/__init__.py",
     f"src/{project_name}/entity/__init__.py",
     f"src/{project_name}/entity/config_entity.py",
-    f"src/{project_name}/constants/__init__/py",
+    f"src/{project_name}/constants/__init__.py",
     "config/config.yaml",
     "params.yaml",
     "schema.yaml",
@@ -39,7 +39,7 @@ for filepath in list_of_files:
     if filedir !="":
         os.makedirs(filedir,exist_ok=True)
         logging.info(f"creating directory;{filedir} for the file: {filename}")
-        
+
     if (not os.path.exists(filepath)) or (os.path.getsize(filepath)==0):
         with open(filepath,"w") as f:
             pass
